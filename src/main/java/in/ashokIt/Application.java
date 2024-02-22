74c9e3a98658a1d75096f49c38904a220747476c
Hrishi7752 <rishijadhav52@gmail.com>
2024-02-22 16:38:43 +0530

    second commit

---
+++ src/main/java/in/ashokIt/Application.java
@@ -0,0 +1,16 @@
+package in.ashokIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
